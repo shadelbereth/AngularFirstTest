314029f2531b9e87f96de1adaa5727451ca6531b
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-05-18 16:55:23 +0200

    Unsubscribe on component destroy

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MovieService} from "../../service/movie.service";
 import {Movie} from "../../model/Movie";
 import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements OnInit, OnDestroy {
   foundMovie: Movie | undefined;
+  private subscription: Subscription | undefined;
 
   constructor(private movieService: MovieService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.movieService.getMovie(Number(id)).subscribe(movieResponse => this.foundMovie = movieResponse);
+    this.subscription = this.movieService.getMovie(Number(id)).subscribe(movieResponse => this.foundMovie = movieResponse);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
 }
--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MovieService} from "../../service/movie.service";
 import {Movie} from "../../model/Movie";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent implements OnInit {
-  movies: Movie[] = []
+export class MovieListComponent implements OnInit, OnDestroy {
+  movies: Movie[] = [];
+  private subscription: Subscription | undefined;
 
   constructor(private movieService:MovieService) { }
 
   ngOnInit(): void {
-    this.movieService.getMovies().subscribe(movieResponse => this.movies = movieResponse);
+    this.subscription = this.movieService.getMovies().subscribe(movieResponse => this.movies = movieResponse);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
 }
