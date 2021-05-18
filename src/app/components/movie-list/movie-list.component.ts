4766a26dd6b04562a3b17f12003a8df2a8e385df
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-05-18 17:04:08 +0200

    Async observables

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,25 +1,20 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MovieService} from "../../service/movie.service";
 import {Movie} from "../../model/Movie";
-import {Subscription} from "rxjs";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent implements OnInit, OnDestroy {
-  movies: Movie[] = [];
-  private subscription: Subscription | undefined;
+export class MovieListComponent implements OnInit {
+  movieObs: Observable<Movie[]> | undefined;
 
-  constructor(private movieService:MovieService) { }
+  constructor(public movieService:MovieService) { }
 
   ngOnInit(): void {
-    this.subscription = this.movieService.getMovies().subscribe(movieResponse => this.movies = movieResponse);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
+    this.movieObs = this.movieService.getMovies();
   }
 
 }
