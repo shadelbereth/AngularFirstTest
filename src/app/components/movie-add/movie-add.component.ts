4db95f1c9384679ea7a641d3ee4f648e4b9d9019
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-05-26 11:13:58 +0200

    Use search and add in movie add component

--- src/app/components/movie-add/movie-add.component.ts
+++ src/app/components/movie-add/movie-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Movie} from "../../model/Movie";
+import {MovieService} from "../../service/movie.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-movie-add',
@@ -7,13 +9,18 @@ import {Movie} from "../../model/Movie";
   styleUrls: ['./movie-add.component.css']
 })
 export class MovieAddComponent implements OnInit {
+  movies: Movie[] = [];
 
-  constructor() { }
+  constructor(private movieService : MovieService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   search(movie : Movie) {
-    console.log(movie);
+    this.movieService.lookupMovie(movie.title).subscribe(movieResponse => this.movies = movieResponse);
+  }
+
+  addNewMovie(movie : Movie) {
+    this.movieService.addMovie(movie.onlineId).subscribe(movieResponse => this.router.navigate(["movie-detail", movieResponse.id]));
   }
 }
