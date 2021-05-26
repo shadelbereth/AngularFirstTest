6f19a489374879c9e51ed35ad3b475fe3e190525
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-05-26 15:42:59 +0200

    Hide delete button (movie list component) if in movie add component

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {MovieService} from "../../service/movie.service";
 import {Movie} from "../../model/Movie";
+import {ActivatedRoute} from "@angular/router";
+import {MovieAddComponent} from "../movie-add/movie-add.component";
 
 @Component({
   selector: 'app-movie-list',
@@ -11,8 +13,11 @@ export class MovieListComponent implements OnInit {
   @Input() movie: Movie[] = [];
   @Output() selected : EventEmitter<Movie> = new EventEmitter<Movie>();
   @Output() delete : EventEmitter<any> = new EventEmitter<any>();
+  hideDelete = false;
 
-  constructor(public movieService:MovieService) { }
+  constructor(public movieService:MovieService, private activatedRoute : ActivatedRoute) {
+    this.hideDelete = activatedRoute.snapshot.component === MovieAddComponent;
+  }
 
   ngOnInit(): void {
   }
