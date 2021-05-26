a28ce3f660a104cd3eb2f4dbf59236c39ce93ccd
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-05-26 09:49:06 +0200

    Use environment to get API url

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Movie } from '../model/Movie';
 import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ export class MovieService {
   constructor(private httpClient: HttpClient) { }
 
   getMovies() {
-    return this.httpClient.get<Movie[]>("https://angularcoursebackend.azurewebsites.net/api/movies");
+    return this.httpClient.get<Movie[]>(environment.apiUrl);
   }
 
   getMovie(id: number) {
-    return this.httpClient.get<Movie>("https://angularcoursebackend.azurewebsites.net/api/movies/" + id);
+    return this.httpClient.get<Movie>(`${environment.apiUrl}/${id}`);
   }
 }
