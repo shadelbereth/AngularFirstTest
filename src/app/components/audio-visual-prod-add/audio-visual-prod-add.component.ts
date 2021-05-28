064abe142de4fe8e62e5601f2933646bd2aa5c78
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-05-28 11:57:57 +0200

    Make Audio Visual Prod Add component generic

--- src/app/components/audio-visual-prod-add/audio-visual-prod-add.component.ts
+++ src/app/components/audio-visual-prod-add/audio-visual-prod-add.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AudioVisualProd} from "../../model/audio-visual-prod";
 
 @Component({
   selector: 'app-audio-visual-prod-add',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./audio-visual-prod-add.component.css']
 })
 export class AudioVisualProdAddComponent implements OnInit {
+  @Input() audioVisualProds: AudioVisualProd[] = [];
+  @Output() search : EventEmitter<NgForm> = new EventEmitter<NgForm>();
+  @Output() add : EventEmitter<AudioVisualProd> = new EventEmitter<AudioVisualProd>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  searchProd(ngForm: NgForm) {
+    this.search.emit(ngForm);
+  }
+
+  goToDetail(prod: AudioVisualProd) {
+    this.add.emit(prod);
+  }
 }
